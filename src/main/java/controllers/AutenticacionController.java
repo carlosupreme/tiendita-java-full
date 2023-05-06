02c1f3c741fc636d88300637b0c3d5735e8c834e
gusama0006@gmail.com <carlos.alberto.sosa.perera@gmail.com>
2023-05-06 16:27:51 -0600

    quitando if del controllador

--- src/main/java/controllers/AutenticacionController.java
+++ src/main/java/controllers/AutenticacionController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import app.Sesion;
-import models.Usuario;
 
 /**
  * 
@@ -14,18 +13,7 @@ public class AutenticacionController {
     
     public boolean login(String username, String password) {
         
-        // Autenticación básica sin acceder a una base de datos
-        
-        if("usuario".equals(username) && "contraseña".equals(password)) {
-            
-            Sesion.instance().setUsuario(
-                    new Usuario("Usuario", "Carlos", "Alberto"));
-            
-            return true;
-            
-        }
-        
-        return false;
+        return true;
         
     }
     
