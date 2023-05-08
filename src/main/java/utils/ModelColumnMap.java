62153911629cee110fd6b2c790cbd2d280c34f5a
raul-itoax <escuela.tecnmoax.raul@gmail.com>
2023-05-08 17:09:26 -0600

    Refactorizacion en mapeo de modelos y columnas

--- src/main/java/utils/ModelColumnMap.java
+++ src/main/java/utils/ModelColumnMap.java
@@ -10,19 +10,19 @@ import java.util.Map;
 
 /**
  *
+ * Esta clase mapea los nombres de atributos de una clase
+ * y los convierte de camelCase a snake_case.
+ * 
  * @author Raul
  */
-public class ModelColumnMapper {
-
-    public static String toSnakeCase(String camelCase) {
-        return camelCase.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
-    }
+public class ModelColumnMap {
 
     public static Map<String, String> getColumns(Class<?> clazz) {
         Map<String, String> columns = new HashMap<>();
         for (Field field : clazz.getDeclaredFields()) {
             String columnName = field.getName();
-            columns.put(field.getName(), toSnakeCase(columnName));
+            columnName = columnName.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
+            columns.put(field.getName(), columnName);
         }
         return columns;
     }
