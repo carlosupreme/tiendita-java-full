6c6f2831c58ef5dce49f764344663475f6bc20b7
raul-itoax <escuela.tecnmoax.raul@gmail.com>
2023-05-08 17:49:44 -0600

    Arreglando mapeo de campos valores

--- src/main/java/utils/ModelColumnMap.java
+++ src/main/java/utils/ModelColumnMap.java
@@ -6,41 +6,37 @@ package utils;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * Esta clase mapea los nombres de atributos de una clase
- * a nombres de columnas de base de datos, lo que significa
- * que debido a las convenciones, los convierte de camelCase 
- * a snake_case.
- * Por ejemplo, convierte "productoId" a "producto_id".
+ * a nombres de columnas de base de datos incluyendo su
+ * respectivo valor. Debido a las convenciones, los atributos
+ * se convierten de camelCase a snake_case. Por ejemplo, convierte 
+ * "productoId" a "producto_id".
  * 
  * @author Raul
  */
 public class ModelColumnMap {
 
-    public static ArrayList<String> getColumns(Class<?> clazz) {
-        ArrayList<String> columnas = new ArrayList<>();
+    public static Map<String, Object> getColumns(Object obj) throws IllegalArgumentException, IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+        
+        Map<String, Object> columnas = new HashMap<>();
         for (Field field : clazz.getDeclaredFields()) {
+            field.setAccessible(true);
+            
             String nombreAtributo = field.getName();
             nombreAtributo = nombreAtributo
                     .replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
-            columnas.add(nombreAtributo);
+            
+            Object fieldValue = field.get(obj);
+            
+            columnas.put(nombreAtributo, fieldValue);
         }
         return columnas;
     }
-    
-    /*public static String getAttrColumnValues() {
-        
-        ArrayList<String> columnas = new ArrayList<>();
-        for (Field field : clazz.getDeclaredFields()) {
-            String nombreAtributo = field.getName();
-            nombreAtributo = nombreAtributo
-                    .replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
-            columnas.add(nombreAtributo);
-        }
-        return columnas;
-        
-    }*/
 
 }
