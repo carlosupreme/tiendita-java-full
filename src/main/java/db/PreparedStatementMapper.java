1f61e106bc16ea53f98f69e8da27c1e1e6cc2411
raul-itoax <escuela.tecnmoax.raul@gmail.com>
2023-05-08 18:45:55 -0600

    añadida logica de consultas preparadas

---
+++ src/main/java/db/PreparedStatementMapper.java
@@ -0,0 +1,77 @@
+package db;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PreparedStatementMapper<T> {
+
+    private final Connection connection = ConexionDB.getInstance().getConnection();
+    private final String tableName;
+
+    public PreparedStatementMapper(Connection connection, String tableName) {
+        this.tableName = tableName;
+    }
+
+    public int insertar(T object) throws SQLException {
+        String query = construirSQLInsert(object);
+        int filasAfectadas;
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            establecerParametrosPreparados(statement, object);
+            filasAfectadas = statement.executeUpdate();
+        }
+        return filasAfectadas;
+    }
+
+    private String construirSQLInsert(T object) {
+        StringBuilder columns = new StringBuilder();
+        StringBuilder values = new StringBuilder();
+        for (Field field : object.getClass().getDeclaredFields()) {
+            if (columns.length() > 0) {
+                columns.append(", ");
+                values.append(", ");
+            }
+            columns.append(field.getName()
+                    .replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase());
+            values.append("?");
+        }
+        return String.format("INSERT INTO %s (%s) VALUES (%s)",
+                tableName, columns.toString(), values.toString());
+    }
+
+    private void establecerParametrosPreparados(PreparedStatement statement, 
+            T object) throws SQLException {
+        Map<String, Object> map = getValoresAtributos(object);
+        for (int i = 1; i <= map.size(); i++) {
+            Object value = map.get("atributo" + i);
+            if (value instanceof String) {
+                statement.setString(i, (String) value);
+            } else if (value instanceof Integer) {
+                statement.setInt(i, (Integer) value);
+            } else if (value instanceof Double) {
+                statement.setDouble(i, (Double) value);
+            } else if (value instanceof Timestamp) {
+                statement.setTimestamp(i, (Timestamp) value);
+            } else if (value instanceof Date) {
+                statement.setDate(i, (Date) value);
+            }
+        }
+    }
+
+    private Map<String, Object> getValoresAtributos(T object) {
+        Map<String, Object> map = new HashMap<>();
+        int i = 1;
+        for (Field field : object.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            try {
+                Object value = field.get(object);
+                map.put("atributo" + i, value);
+                i++;
+            } catch (IllegalAccessException e) {
+                // Ignorar campos inaccesibles
+            }
+        }
+        return map;
+    }
+}
