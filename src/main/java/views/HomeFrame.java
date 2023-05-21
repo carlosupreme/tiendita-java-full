fb9fac9d677b500cc7172c42f96bf6230d8c62d3
raul-itoax <escuela.tecnmoax.raul@gmail.com>
2023-05-21 17:20:59 -0600

    clase de vista detalles venta agregada

--- src/main/java/views/HomeFrame.java
+++ src/main/java/views/HomeFrame.java
@@ -192,57 +192,7 @@ public class HomeFrame extends javax.swing.JFrame {
 
     private void ventasBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ventasBtnActionPerformed
 
-        SelectStatementMapper<DetallesVenta> mapper
-                = new SelectStatementMapper<>("detalles_venta");
-
-        //HashMap<String, String> mapeosAttr = new HashMap<>();
-        //mapeosAttr.put("idProducto", "Ver detalles");
-        //mapper.setMapeoAtributos(mapeosAttr);
-        try {
-            String[][] datos = mapper.selectAllAsArray(DetallesVenta.class, new String[]{});
-
-            String[] columnasTabla = {"ID de venta", "ID del producto", "Cantidad", "Precio Unitario"};
-
-            DefaultTableModel modelo = new DefaultTableModel(datos, columnasTabla);
-            modelo.setDataVector(datos, columnasTabla);
-
-            JTable tabla = new JTable(modelo);
-
-            Action detallesProductoBtn;
-            detallesProductoBtn = new AbstractAction() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JTable table = (JTable) e.getSource();
-                    int modelRow = Integer.parseInt(e.getActionCommand());
-                    Object valor = ((DefaultTableModel) table.getModel()).getValueAt(modelRow, 1);
-                    JOptionPane.showMessageDialog(null, "Detalles del producto con id = "
-                            + valor.toString());
-                }
-            };
-
-            ButtonColumn btnDetalles1 = new ButtonColumn(tabla, detallesProductoBtn, 1,
-                    new ImageIcon(getClass().getResource("/info_icon.png")));
-            
-            tabla.setRowHeight(30);
-
-            JFrame f = new JFrame();
-            f.setSize(800, 500);
-            f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-            JPanel panel = new JPanel();
-            panel.setLayout(new BorderLayout());
-            JScrollPane scroll = new JScrollPane(tabla);
-            panel.add(scroll, BorderLayout.CENTER);
-            f.add(panel);
-
-            f.setVisible(true);
-
-        } catch (IllegalAccessException | IllegalArgumentException
-                | InstantiationException | NoSuchMethodException
-                | InvocationTargetException | SQLException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, ex.getMessage());
-        }
+        
 
     }//GEN-LAST:event_ventasBtnActionPerformed
 
---
+++ src/main/java/views/VistaDetallesVenta.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package views;
+
+import db.SelectStatementMapper;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import models.DetallesVenta;
+
+/**
+ *
+ * @author Raul
+ */
+public class VistaDetallesVenta {
+    
+    private int idVenta;
+
+    public VistaDetallesVenta(int idVenta) {
+        
+        SelectStatementMapper<DetallesVenta> ventaMap
+                = new SelectStatementMapper<>("detalles_venta");
+        
+        ventaMap.setSql("SELECT detalles_venta.* FROM ventas INNER JOIN "
+                + "detalles_venta ON ventas.id = detalles_venta.id_venta "
+                + "WHERE detalles_venta.id_venta = " + idVenta);
+        
+        try {
+            String[][] datos = ventaMap.selectAllAsArray(DetallesVenta.class, new String[]{});
+
+            String[] columnasTabla = {"ID de venta", "ID del producto", "Cantidad", "Precio Unitario"};
+
+            DefaultTableModel modelo = new DefaultTableModel(datos, columnasTabla);
+            modelo.setDataVector(datos, columnasTabla);
+
+            JTable tabla = new JTable(modelo);
+
+            Action detallesProductoBtn;
+            detallesProductoBtn = new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JTable table = (JTable) e.getSource();
+                    int modelRow = Integer.parseInt(e.getActionCommand());
+                    Object valor = ((DefaultTableModel) table.getModel()).getValueAt(modelRow, 1);
+                    JOptionPane.showMessageDialog(null, "Detalles del producto con id = "
+                            + valor.toString());
+                }
+            };
+
+            ButtonColumn btnDetalles1 = new ButtonColumn(tabla, detallesProductoBtn, 1,
+                    new ImageIcon(getClass().getResource("/info_icon.png")));
+            
+            tabla.setRowHeight(30);
+
+            JFrame f = new JFrame();
+            f.setSize(800, 500);
+            f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+            JScrollPane scroll = new JScrollPane(tabla);
+            panel.add(scroll, BorderLayout.CENTER);
+            f.add(panel);
+
+            f.setVisible(true);
+
+        } catch (IllegalAccessException | IllegalArgumentException
+                | InstantiationException | NoSuchMethodException
+                | InvocationTargetException | SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        
+    }
+    
+    
+    
+}
