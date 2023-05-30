f737c4aaa7c5a6e90fbe920d0bf65ec3664460a3
raul-itoax <escuela.tecnmoax.raul@gmail.com>
2023-05-29 21:08:37 -0600

    agregando estilo botones

--- src/main/java/app/AdministradorTiendita.java
+++ src/main/java/app/AdministradorTiendita.java
@@ -1,5 +1,6 @@
 package app;
 
+import javax.swing.UIManager;
 import views.LoginFrame;
 
 /**
@@ -11,6 +12,16 @@ import views.LoginFrame;
 public class AdministradorTiendita {
 
     public static void main(String[] args) {
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+        }
 
         java.awt.EventQueue.invokeLater(() -> {
 
