b919fbeb9801b930bf6d8698de83e12dab18af81
Ilian Martínez <ili03maro@gmail.com>
2023-05-30 00:41:49 -0600

    clases para filtrar

---
+++ src/main/java/repositories/ProveedorCriteria.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositories;
+
+/**
+ *
+ * @author ili
+ */
+public class ProveedorCriteria {
+
+    public String nombre;
+    public String direccion;
+    public String email;
+    public String telefono;
+
+    public ProveedorCriteria() {
+        this.nombre = "";
+        this.direccion = "";
+        this.email = "";
+        this.telefono = "";
+    }
+
+}
