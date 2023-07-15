f0d03eb329999312904dfb078f099863a3851b85
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2023-07-15 17:46:04 -0600

    implementing in infrastructure layer

--- src/main/java/supplier/infrastructure/MySQLSupplierRepository.java
+++ src/main/java/supplier/infrastructure/MySQLSupplierRepository.java
@@ -1,12 +1,15 @@
 package supplier.infrastructure;
 
-import shared.domain.valueobject.criteria.Criteria;
-import supplier.domain.SupplierNotExist;
-import supplier.domain.entities.*;
-
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import shared.domain.valueobject.DomainError;
+import shared.domain.valueobject.criteria.Criteria;
+import shared.domain.valueobject.criteria.Operator;
+import shared.domain.valueobject.criteria.Order;
+import supplier.domain.SupplierNotExist;
+import supplier.domain.entities.*;
 
 public class MySQLSupplierRepository implements SupplierRepository {
 
@@ -61,7 +64,7 @@ public class MySQLSupplierRepository implements SupplierRepository {
             SupplierEmail email = new SupplierEmail(rs.getString("email"));
             SupplierPhone phone = new SupplierPhone(rs.getString("telefono"));
 
-            return Optional.of(new Supplier(name, address, email, phone));
+            return Optional.of(new Supplier(id, name, address, email, phone));
 
         } catch (SQLException | SupplierNotExist e) {
             e.printStackTrace();
@@ -72,17 +75,92 @@ public class MySQLSupplierRepository implements SupplierRepository {
 
     @Override
     public void update(SupplierId id, Supplier supplier) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            PreparedStatement st = connection.prepareStatement("UPDATE proveedores SET nombre = ?, direccion = ?, email = ?,  telefono = ?  WHERE id = ?");
+            st.setString(1, supplier.name().value());
+            st.setString(2, supplier.address().value());
+            st.setString(3, supplier.email().value());
+            st.setString(4, supplier.phoneNumber().value());
+            st.setLong(5, id.value());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DomainError("No se actualizo el proveedor");
+        }
     }
 
     @Override
     public List<Supplier> matching(Criteria criteria) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<Supplier> suppliers = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            StringBuilder sql = new StringBuilder("SELECT * FROM proveedores WHERE activo = 1");
+
+            if (criteria.hasFilters()) {
+                criteria.filters().filters().forEach(filter -> {
+                    sql.append(" AND ").append(filter.field()).append(" ");
+
+                    if (filter.operator().equals(Operator.LIKE.getValue())) {
+                        sql.append("LIKE '%").append(filter.value()).append("%'");
+                    } else {
+                        sql.append(filter.operator()).append(" '").append(filter.value()).append("'");
+                    }
+                });
+            }
+
+            if (criteria.hasOrder()) {
+                Order order = criteria.order();
+                sql.append(" ORDER BY ").append(order.orderBy()).append(" ").append(order.orderType());
+            }
+
+            if (criteria.hasLimit()) {
+                sql.append(" LIMIT ").append(criteria.limit());
+            }
+
+            if (criteria.hasOffset()) {
+                sql.append(" OFFSET ").append(criteria.offset());
+            }
+
+            System.out.println(sql);
+
+            ResultSet rs = stmt.executeQuery(sql.toString());
+
+            while (rs.next()) {
+                suppliers.add(
+                        new Supplier(
+                                new SupplierId(rs.getLong("id")),
+                                new SupplierName(rs.getString("nombre")),
+                                new SupplierAddress(rs.getString("direccion")),
+                                new SupplierEmail(rs.getString("email")),
+                                new SupplierPhone(rs.getString("telefono"))
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return suppliers;
     }
 
     @Override
     public Optional<Supplier> deleteById(SupplierId id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+
+            Optional<Supplier> supplier = this.findById(id);
+
+            if (supplier.isEmpty()) {
+                throw new Exception("No se encontró el proveedor");
+            }
+
+            PreparedStatement st = connection.prepareStatement("UPDATE proveedores SET activo = 0 WHERE id = ? LIMIT 1");
+            st.setLong(1, id.value());
+            st.executeUpdate();
+
+            return supplier;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
     }
 
 }
